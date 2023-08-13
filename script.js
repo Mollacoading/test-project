ea8bde89a79bbe8330167810974d0a0eac5011e9
Mollacoading <ashimmolla25@gmail.com>
2023-08-13 20:41:16 +0600

    this is modified

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementById('html').innerHTML = This is my firstName;
+/* function to(fistName, lastName){
+    console.log(fistName / lastName);
+}
+
+to(10, 11); */
+
+function math(number){
+   return number + number;
+}
+console.log(math(8));
